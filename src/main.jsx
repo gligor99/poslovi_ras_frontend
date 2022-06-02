6721720d352b86462e88ebf3d9f85e9e8f5d436d
gligor99 <lukagligorevic@gmail.com>
2022-06-02 11:34:35 +0100

    Another conflict fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-<<<<<<< HEAD
+
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-=======
->>>>>>> 4133f1ccfcaea5dc7ad46b3c4ce4ec6351d5086c
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
