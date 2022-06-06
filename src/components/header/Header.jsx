635404fcf5678bf70511511c2bd27c685ceb9943
Milos Stojanovic <stojanovic98m@gmail.com>
2022-06-06 12:34:29 +0200

    SearchBar import fix & structue update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 // Components
-import { SearchBar } from "./SearchBar";
+import SearchBar from "./SearchBar";
 
 // MUI Components
 import Box from "@mui/material/Box";
--- src/components/main_content/MainContent.jsx
+++ src/components/main_content/MainContent.jsx
@@ -1,12 +1,20 @@
 import Jobs from "./Jobs";
 import Companies from "./Companies";
 
+import Container from "@mui/material/Container";
+
 const MainContent = () => {
   return (
-    <>
+    <Container
+      maxWidth="xl"
+      sx={{
+        backgroundColor: "red",
+        height: "",
+      }}
+    >
       <Jobs />
       <Companies />
-    </>
+    </Container>
   );
 };
 
