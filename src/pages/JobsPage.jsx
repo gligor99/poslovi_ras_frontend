d5967c9c04c93fc1fe8ba1b2b3c1e12e8173be4e
Milos Stojanovic <stojanovic98m@gmail.com>
2022-06-13 11:13:42 +0200

    grid layout fixed

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -96,7 +96,7 @@ const JobsPage = () => {
           }}
         >
           <Grid container spacing={2} sx={{ justifyContent: "space-evenly" }}>
-            <Grid item xs={5}>
+          <Grid item xs={5}>
               <Card
                 sx={{
                   height: "100%",
@@ -123,130 +123,9 @@ const JobsPage = () => {
                   <Box
                     component="img"
                     sx={{
-                      height: "min-content",
-                      width: "100%",
-                      backgroundRepeat: "no-repeat",
-                      backgroundPosition: "center",
-                      backgroundSize: "cover",
-                    }}
-                    src={logo}
-                  ></Box>
-                </Box>
-
-                <Box
-                  sx={{
-                    height: "100%",
-                    width: "50%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    pl: "10px",
-                    pr: "5px",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      height: "80%",
-                      width: "100%",
-                    }}
-                  >
-                    <Typography
-                      variant="h6"
-                      sx={{
-                        height: "20%",
-                        width: "100%",
-                        fontSize: "1rem",
-                        fontFamily: "Roboto",
-                        color: "black",
-                        textAlign: "center",
-                      }}
-                    >
-                      D.O.O Naziv firme dwqdw
-                    </Typography>
-                    <Typography
-                      variant="p"
-                      sx={{
-                        height: "80%",
-                        width: "100%",
-                        fontFamily: "Roboto",
-                      }}
-                    >
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Optio magni officiis tempora laborum similique deleniti
-                      provident distinctio minus nemo rerum?
-                    </Typography>
-                  </Box>
-                </Box>
-                <Box
-                  sx={{
-                    height: "100%",
-                    width: "20%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      height: "80%",
-                      width: "95%",
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "flex-end",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Button
-                      size="small"
-                      sx={{
-                        color: "white",
-                        backgroundColor: "primary.main",
-                        width: "80%",
-                        height: "20%",
-                        "&:hover": {
-                          color: "white",
-                          backgroundColor: "primary.main",
-                          filter: "brightness(70%)",
-                        },
-                      }}
-                    >
-                      Detalji
-                    </Button>
-                  </Box>
-                </Box>
-              </Card>
-            </Grid>
-            <Grid item xs={5}>
-              <Card
-                sx={{
-                  height: "100%",
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "flex-start",
-                  bgcolor: "white",
-                  pr: "5px",
-                  boxShadow: 1,
-                }}
-              >
-                <Box
-                  sx={{
-                    height: "100%",
-                    width: "30%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Box
-                    component="img"
-                    sx={{
-                      height: "min-content",
+                      
                       width: "80%",
+                      height: "80%",
                       backgroundRepeat: "no-repeat",
                       backgroundPosition: "center",
                       backgroundSize: "cover",
@@ -340,6 +219,131 @@ const JobsPage = () => {
                 </Box>
               </Card>
             </Grid>
+            <Grid item xs={5}>
+              <Card
+                sx={{
+                  height: "100%",
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "flex-start",
+                  bgcolor: "white",
+                  pr: "5px",
+                  boxShadow: 1,
+                }}
+              >
+                <Box
+                  sx={{
+                    height: "100%",
+                    width: "30%",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Box
+                    component="img"
+                    sx={{
+                      
+                      width: "80%",
+                      height: "80%",
+                      backgroundRepeat: "no-repeat",
+                      backgroundPosition: "center",
+                      backgroundSize: "cover",
+                    }}
+                    src={logo}
+                  ></Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    height: "100%",
+                    width: "50%",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    pl: "10px",
+                    pr: "5px",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      height: "80%",
+                      width: "100%",
+                    }}
+                  >
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        height: "20%",
+                        width: "100%",
+                        fontSize: "1rem",
+                        fontFamily: "Roboto",
+                        color: "black",
+                        textAlign: "center",
+                      }}
+                    >
+                      D.O.O Naziv firme dwqdw
+                    </Typography>
+                    <Typography
+                      variant="p"
+                      sx={{
+                        height: "80%",
+                        width: "100%",
+                        fontFamily: "Roboto",
+                      }}
+                    >
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Optio magni officiis tempora laborum similique deleniti
+                      provident distinctio minus nemo rerum?
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box
+                  sx={{
+                    height: "100%",
+                    width: "20%",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      height: "80%",
+                      width: "95%",
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "flex-end",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Button
+                      size="small"
+                      sx={{
+                        color: "white",
+                        backgroundColor: "primary.main",
+                        width: "80%",
+                        height: "20%",
+                        "&:hover": {
+                          color: "white",
+                          backgroundColor: "primary.main",
+                          filter: "brightness(70%)",
+                        },
+                      }}
+                    >
+                      Detalji
+                    </Button>
+                  </Box>
+                </Box>
+              </Card>
+            </Grid>
+            
+            
           </Grid>
         </Box>
       </Container>
