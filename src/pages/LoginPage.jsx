3bb992fbea8ef62732f8a5fba744b24e90e5a02c
Milos Stojanovic <stojanovic98m@gmail.com>
2022-06-14 10:36:36 +0200

    login & register styles updated

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -13,66 +13,113 @@ import Container from "@mui/material/Container";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 const LoginPage = () => {
   return (
-    <Container component="main" maxWidth="xs">
+    <Container
+      component="main"
+      maxWidth="sm"
+      sx={{
+        height: "900px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <CssBaseline />
       <Box
         sx={{
-          height: "900px",
+          p: 3,
+          height: "700px",
           width: "100%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
+          backgroundColor: "background.main",
+          boxShadow: 0,
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Prijavite se
-        </Typography>
-        <Box component="form" noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email adresa"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Lozinka"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
+        <Box
+          sx={{
+            p: 5,
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "white",
+            boxShadow: 1,
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Prijavite se
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email adresa"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Lozinka"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Prijava
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              {/* <Link  variant="body2">
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Prijava
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                {/* <Link  variant="body2">
                   Zaboravili ste lozinku?
                 </Link> */}
+              </Grid>
+              <Grid item>
+                <Box
+                  sx={{
+                    display: "flex",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      pr: 1,
+                    }}
+                  >
+                    Nemate nalog?{" "}
+                  </Typography>
+                  <Link
+                    to="/register"
+                    variant="body2"
+                    style={{
+                      textDecoration: "none",
+                      color: "red",
+                    }}
+                  >
+                    Registrujte se
+                  </Link>
+                </Box>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link to="/register" variant="body2">
-                {"Nemate nalog? Registrujte se."}
-              </Link>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
       </Box>
     </Container>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -19,134 +19,182 @@ import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 
 const RegisterPage = () => {
   return (
-    <Container component="main" maxWidth="xs">
+    <Container
+      component="main"
+      maxWidth="sm"
+      sx={{
+        height: "900px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <CssBaseline />
       <Box
         sx={{
-          height: "900px",
+          p: 3,
+          height: "700px",
           width: "100%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
+          backgroundColor: "background.main",
+          boxShadow: 0,
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <AppRegistrationIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Registracija
-        </Typography>
-        <Box component="form" noValidate sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="given-name"
-                name="companyName"
-                required
-                fullWidth
-                id="companyName"
-                label="Naziv kompanije"
-                autoFocus
-              />
-            </Grid>
+        <Box
+          sx={{
+            p: 5,
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "white",
+            boxShadow: 1,
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <AppRegistrationIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Registracija
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="companyName"
+                  required
+                  fullWidth
+                  id="companyName"
+                  label="Naziv kompanije"
+                  autoFocus
+                />
+              </Grid>
 
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Email adresa"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                label="Lozinka"
-                type="password"
-                id="password"
-                autoComplete="new-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextareaAutosize
-                aria-label="about company"
-                minRows={4}
-                placeholder="Dodajte opis (opcionalno)"
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email adresa"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Lozinka"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextareaAutosize
+                  aria-label="about company"
+                  minRows={4}
+                  placeholder="Dodajte opis (opcionalno)"
+                  style={{
+                    width: "70%",
+                    outline: "1px solid lightgray",
+                    fontSize: "0.8rem",
+                    padding: "10px",
+                  }}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
                 style={{
-                  width: "70%",
-                  outline: "1px solid lightgray",
-                  fontSize: "0.8rem",
-                  padding: "10px",
-                }}
-              />
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              style={{
-                paddingTop: "0.3rem",
-                paddingBottom: "0.3rem",
-              }}
-            >
-              <Stack
-                direction="row"
-                alignItems="center"
-                sx={{
-                  height: "100%",
+                  paddingTop: "0.3rem",
+                  paddingBottom: "0.3rem",
                 }}
               >
-                <Typography
+                <Stack
+                  direction="row"
+                  alignItems="center"
                   sx={{
-                    pr: 1,
+                    height: "100%",
                   }}
                 >
-                  {" "}
-                  Dodaj sliku
-                </Typography>
-                <label htmlFor="icon-button-file">
-                  <Input
+                  <Typography
                     sx={{
-                      display: "none",
-                    }}
-                    accept="image/*"
-                    id="icon-button-file"
-                    type="file"
-                  />
-                  <IconButton
-                    color="primary"
-                    aria-label="upload picture"
-                    component="span"
-                    sx={{
-                      margin: 0,
-                      padding: 0,
+                      pr: 1,
                     }}
                   >
-                    <PhotoCamera
+                    {" "}
+                    Dodaj sliku
+                  </Typography>
+                  <label htmlFor="icon-button-file">
+                    <Input
                       sx={{
-                        height: "30px",
-                        width: "30px",
+                        display: "none",
                       }}
+                      accept="image/*"
+                      id="icon-button-file"
+                      type="file"
                     />
-                  </IconButton>
-                </label>
-              </Stack>
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="span"
+                      sx={{
+                        margin: 0,
+                        padding: 0,
+                      }}
+                    >
+                      <PhotoCamera
+                        sx={{
+                          height: "30px",
+                          width: "30px",
+                        }}
+                      />
+                    </IconButton>
+                  </label>
+                </Stack>
+              </Grid>
             </Grid>
-          </Grid>
-          <Button type="submit" fullWidth variant="contained" sx={{ mb: 2 }}>
-            Registruj se
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link to="/login" variant="body2">
-                Imate nalog? Prijavite se.
-              </Link>
+            <Button type="submit" fullWidth variant="contained" sx={{ mb: 2 }}>
+              Registruj se
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Box
+                  sx={{
+                    display: "flex",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      pr: 1,
+                    }}
+                  >
+                    {" "}
+                    Imate nalog?{" "}
+                  </Typography>
+                  <Link
+                    to="/login"
+                    variant="body2"
+                    style={{
+                      textDecoration: "none",
+                      color: "red",
+                    }}
+                  >
+                    Prijavite se.
+                  </Link>
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
+          </Box>
         </Box>
       </Box>
     </Container>
